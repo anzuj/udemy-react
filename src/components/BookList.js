99a3825ea765f8a899bd545d4dd932db31e3bfe9
anzuj <anzelika.haeberli@gmail.com>
2023-03-21 22:49:29 +0100

    prepare context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,16 @@
-import { useState } from "react";
 import BookShow from "./BookShow";
+import { useContext } from "react";
+import BooksContext from "../context/books";
 
 function BookList({ books, onDelete , onEdit}) {
+  const {count, incrementCount} = useContext(BooksContext)
+
   const renderedBooks = books.map((book) => {
     return <BookShow book={book} key={book.id} onDelete={onDelete} onEdit={onEdit}/>;
   });
-  return <div className="book-list">{renderedBooks}</div>;
+  return <div className="book-list">
+    <div>{count} <br/> <button onClick={incrementCount}>Click</button> </div>
+    {renderedBooks}</div>;
 }
 
 export default BookList;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+import { Provider } from "./context/books";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider >
+    <App />
+  </Provider>
+);
